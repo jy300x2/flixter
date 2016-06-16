c6aeb909d25492399f6079a6c27126b5aa9085ea
Jon Yu <jy300x2@gmail.com>
2016-06-16 15:36:39 +0000

    Edited authentication

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
   end
 end
