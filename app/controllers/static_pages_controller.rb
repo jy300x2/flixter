fea7903781505c46535e0d9a0a44c40727725194
Jon Yu <jy300x2@gmail.com>
2016-07-01 03:26:41 +0000

    Added privacy policy

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,7 @@ class StaticPagesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
   end
+
+  def privacy
+  end
 end
