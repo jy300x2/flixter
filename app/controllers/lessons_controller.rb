54ca82091bc120e40b468357c4e7940c34af1811
Jon Yu <jy300x2@gmail.com>
2016-07-01 03:07:32 +0000

    Added next lesson button

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,11 +8,16 @@ class LessonsController < ApplicationController
   private
 
   def require_authorized_for_current_course
-    this_course = current_lesson.section.course
-    if !current_lesson.section.course.user.enrolled_in?(this_course)
-      redirect_to course_path(this_course), alert: 'Please enroll in course to access all content'
+    @course = current_lesson.section.course
+    unless current_user.enrolled_in?(@course) 
+      redirect_to course_path(@course), alert: 'You must be enrolled in this lesson.'
     end
   end
+    # this_course = current_lesson.section.course
+    # unless current_lesson.section.course.user.enrolled_in?(this_course)
+    #   redirect_to course_path(this_course), alert: 'Please enroll in course to access all content'
+    # end
+
 
   helper_method :current_lesson
   def current_lesson
